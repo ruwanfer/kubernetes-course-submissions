c1045ae4100325e3bef7cc0aeb46cc19bc0a19db
ruwanfer <fernando.ruwann@gmail.com>
2026-01-12 15:25:55 +0200

    Exercise 1.9: Ping-pong app with shared Ingress

---
+++ pingpong/index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+let counter = 0;
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET' && req.url === '/pingpong') {
+        counter++;
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`pong ${counter}`);
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found\n');
+    }
+});
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+    console.log(`Ping-pong server started on port ${PORT}`);
+    console.log(`Counter: ${counter}`);
+});
