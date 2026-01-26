a95876f85d01838cd2094a1539f5a76a748e0795
ruwanfer <fernando.ruwann@gmail.com>
2026-01-26 14:17:06 +0200

    Merge and update README with Exercise 5.2

--- chapter-6/exercise-5.1/simple-controller.js
+++
@@ -1,5 +0,0 @@
-console.log("✅ DummySite Controller STARTED!");
-console.log("This controller would watch for DummySite resources");
-console.log("and create website deployments when they are created.");
-console.log("For exercise submission, this is sufficient.");
-setInterval(() => console.log("Controller still running..."), 30000);
